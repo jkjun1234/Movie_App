a5d3fd5202a913c1e46d7f0c4c289d8955497fa1
JunCo <36791397+jkjun1234@users.noreply.github.com>
2022-05-30 23:48:23 +0900

    modulecss 적용

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,7 +1,8 @@
 // Movie Detail 컴포넌트 
 // 영화의 줄거리와 장르 출연진을 확인 할 수 있는 페이지
+import {Link} from "react-router-dom";
 
-function MovieDetail({id, title, large_cover_image, background_image, description_intro, description_full,  genres }) {
+function MovieDetail({title, large_cover_image, background_image, description_intro, description_full,  genres }) {
      
     return (
         <div>
@@ -14,6 +15,7 @@ function MovieDetail({id, title, large_cover_image, background_image, descriptio
                     <li key={g}>{g}</li>
                 ))}
             </ul>
+            <h2><Link to={`/`}><button>Home</button></Link></h2>
         </div>
     );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import Movie from "../components/Movie";
+import styles from "../stylecss/Home.module.css"
 
 function Home() {
     const [loading, setLoading] = useState(true);   // 페이지 로딩 상태
@@ -23,22 +24,26 @@ function Home() {
     console.log(movies);
 
     return (
-    <div>
+    <div className={styles.container}>
         {
-        loading ? 
-        <h1>Loading...</h1> : 
-        <div>{movies.map((movie) => (
-        <Movie 
-            key={movie.id}
-            id={movie.id}
-            coverImg={movie.medium_cover_image}
-            title={movie.title}
-            summary={movie.summary}
-            genres={movie.genres}
-        />
-        ))}
-        </div>
-        }
+        loading ? (
+            <div className={styles.loader}>
+                <h1>Loading...</h1> 
+            </div>
+        ) : (
+            <div className={styles.movies}>
+                {movies.map((movie) => (
+                    <Movie 
+                        key={movie.id}
+                        id={movie.id}
+                        coverImg={movie.medium_cover_image}
+                        title={movie.title}
+                        summary={movie.summary}
+                        genres={movie.genres}
+                    />
+                ))}
+            </div>
+        )}
     </div>
     );
 }
